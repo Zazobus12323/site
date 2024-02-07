0d59d3358acc75415d857f2d7f8b28d6aed86e41
maqfer <107036045+maqfer@users.noreply.github.com>
2024-02-07 13:27:44 +0300

    all

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     const buttonSUV = document.querySelector('button[class="car_SUV"]')
+    const buttonSUVStyle = window.getComputedStyle(buttonSUV)
     const buttonSportcar = document.querySelector('button[class="car_sportcar"]')
     const buttonSportcarStyle = window.getComputedStyle(buttonSportcar)
     const buttonBusiness = document.querySelector('button[class="car_business"]')
+    const buttonBusinessStyle = window.getComputedStyle(buttonBusiness)
     const buttonPremium = document.querySelector('button[class="car_premium"]')
+    const buttonPremiumStyle = window.getComputedStyle(buttonPremium)
     const buttonComfort = document.querySelector('button[class="car_comfort"]')
+    const buttonComfortStyle = window.getComputedStyle(buttonComfort)
     buttonSUV.addEventListener('click', () => {
-        if(buttonSUV.style.backgroundColor === 'rgb(99, 99, 99)'){
+        if(buttonSUVStyle.getPropertyValue('background-color') === 'rgb(99, 99, 99)'){
             buttonSUV.style.backgroundColor = 'rgb(255, 255, 255)'
-            bchagne(buttonSUV, buttons).map((el) => el.backgroundColor = 'rgb(99, 99, 99)')
+            buttonSportcar.style.backgroundColor = 'rgb(99, 99, 99)'
+            buttonBusiness.style.backgroundColor = 'rgb(99, 99, 99)'
+            buttonPremium.style.backgroundColor = 'rgb(99, 99, 99)'
+            buttonComfort.style.backgroundColor = 'rgb(99, 99, 99)'
+            
         }
 })
     buttonSportcar.addEventListener('click', ()=>{
@@ -17,6 +25,33 @@ document.addEventListener('DOMContentLoaded', ()=>{
             buttonBusiness.style.backgroundColor = 'rgb(99, 99, 99)'
             buttonPremium.style.backgroundColor = 'rgb(99, 99, 99)'
             buttonComfort.style.backgroundColor = 'rgb(99, 99, 99)'
+            buttonSUV.style.backgroundColor = 'rgb(99, 99, 99)'
+        }
+    })
+    buttonBusiness.addEventListener('click', ()=>{
+        if(buttonBusinessStyle.getPropertyValue('background-color') === 'rgb(99, 99, 99)'){
+            buttonSportcar.style.backgroundColor = 'rgb(99, 99, 99)'
+            buttonBusiness.style.backgroundColor = 'rgb(255, 255, 255)'
+            buttonPremium.style.backgroundColor = 'rgb(99, 99, 99)'
+            buttonComfort.style.backgroundColor = 'rgb(99, 99, 99)'
+            buttonSUV.style.backgroundColor = 'rgb(99, 99, 99)'
+        }
+    })
+    buttonPremium.addEventListener('click', () =>{
+        if(buttonPremiumStyle.getPropertyValue('background-color') === 'rgb(99, 99, 99)'){
+            buttonSportcar.style.backgroundColor = 'rgb(99, 99, 99)'
+            buttonBusiness.style.backgroundColor = 'rgb(99, 99, 99)'
+            buttonPremium.style.backgroundColor = 'rgb(255, 255, 255)'
+            buttonComfort.style.backgroundColor = 'rgb(99, 99, 99)'
+            buttonSUV.style.backgroundColor = 'rgb(99, 99, 99)'
+        }
+    })
+    buttonComfort.addEventListener('click', ()=>{
+        if(buttonComfortStyle.getPropertyValue('background-color') === 'rgb(99, 99, 99)'){
+            buttonSportcar.style.backgroundColor = 'rgb(99, 99, 99)'
+            buttonBusiness.style.backgroundColor = 'rgb(99, 99, 99)'
+            buttonPremium.style.backgroundColor = 'rgb(99, 99, 99)'
+            buttonComfort.style.backgroundColor = 'rgb(255, 255, 255)'
             buttonSUV.style.backgroundColor = 'rgb(99, 99, 99)'
         }
     })
